d0c60c7ed18cfa4ff760f7051cc9557d8339cff8
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-11-21 14:26:33 +0100

    chore: add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
